e9089d0918046e45481ca4e1e55a608187457623
Lucas Weinstein <lucasweinstein@Lucass-MBP.lan>
2025-11-07 15:25:56 -0800

    Fixing client

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 const router = require("./routes");
